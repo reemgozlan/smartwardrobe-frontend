0e5c9c10378f6a99ac0fd243012cf175acbb56d1
reemgozlan <118187653+reemgozlan@users.noreply.github.com>
2023-03-08 14:18:10 +0100

    pushing the app

--- src/App.js
+++ src/App.js
@@ -8,29 +8,11 @@ import {Routes, Route} from "react-router-dom";
 import Header from "./components/Header"
 import Footer from "./components/Footer";
 import MakeanOutfit from "./components/MakeanOutfit";
-import { useMediaQuery } from 'react-responsive'
+
 
 
 function App() {
-  const isMobileDevice = useMediaQuery({
-    query: "(min-device-width: 480px)",
-  });
-
-  const isTabletDevice = useMediaQuery({
-    query: "(min-device-width: 768px)",
-  });
-
-  const isLaptop = useMediaQuery({
-    query: "(min-device-width: 1024px)",
-  });
-
-  const isDesktop = useMediaQuery({
-    query: "(min-device-width: 1200px)",
-  });
-
-  const isBigScreen = useMediaQuery({
-    query: "(min-device-width: 1201px )",
-  });
+  
   return (
     <div className="App">
       <h1></h1>
